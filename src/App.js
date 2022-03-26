3b058cee0ba286b450521ef4dd8578bcc646cd77
Natalia1919 <gn.natalia19@gmail.com>
2022-03-26 19:57:06 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+   <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>
+          Content
+        </main>
+      </div>
+   </Router>
   );
 }
 
