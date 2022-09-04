d6a3595fde0b4cca28d22083c3ab2eb937ac13ea
liaHi19 <gn.natalia19@gmail.com>
2022-09-04 10:40:33 +0300

    Add error Alert functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-import {GithubProvider} from "./context/github/GithubContext";
+import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
+import Alert from "./components/layout/Alert";
 
 function App() {
   return (
-  <GithubProvider>
-   <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" exact element={<Home/>} />
-            <Route path="/about"  element={<About/>} />
-            <Route path="/*" exact element={<NotFound/>} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-   </Router>
-   </GithubProvider>
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/*" exact element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   );
 }
 
--- src/components/layout/users/UserSearch.jsx
+++ src/components/layout/users/UserSearch.jsx
@@ -1,37 +1,49 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext, useEffect } from "react";
 import GithubContext from "../../../context/github/GithubContext";
+import AlertContext from "../../../context/alert/AlertContext";
 
 const UserSearch = () => {
   const [text, setText] = useState("");
 
-  const {users, searchUsers, clearUsers} = useContext(GithubContext);
+  const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert, removeAlert } = useContext(AlertContext);
 
-  const handleChange = (e) => {setText(e.target.value)};
+  useEffect(() => {
+    setTimeout(() => {
+      removeAlert();
+    }, 3000);
+  }, [setAlert]);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(text === ""){
-      alert("The search can't be empty")
-    }
-    else{
-      searchUsers(text)
+    if (text === "") {
+      setAlert("The search can't be empty", "error");
+    } else {
+      searchUsers(text);
       setText("");
     }
-
-  }
+  };
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-8 mb-8">
       <div>
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
-              <input type="text" 
-              className="w-full pr-40 bg-gray-200 input input-lg text-black" 
-              placeholder="Search"
-              value={text}
-              onChange={handleChange}/>
-              <button className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
-              type="submit">
+              <input
+                type="text"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                placeholder="Search"
+                value={text}
+                onChange={handleChange}
+              />
+              <button
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
+                type="submit"
+              >
                 Go
               </button>
             </div>
@@ -40,14 +52,13 @@ const UserSearch = () => {
       </div>
       {users.length > 0 && (
         <div>
-        <button className="btn btn-ghost btn-lg"
-        onClick={clearUsers}>
-         Clear
-        </button>
+          <button className="btn btn-ghost btn-lg" onClick={clearUsers}>
+            Clear
+          </button>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default UserSearch
+export default UserSearch;
